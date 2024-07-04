46bcc3c5aaa037dd90df16ab8f34c2c3234c699e
Lucas Cesar Ferreira Domingos <cesa0007@flinders.edu.au>
2024-07-04 10:27:47 +0930

    Formatting with black and line-length of 100

--- models/encoders/encoders.py
+++ models/encoders/encoders.py
@@ -3,24 +3,26 @@ import torch
 
 models = {}
 
+
 def register(name):
-  def decorator(cls):
-    models[name] = cls
-    return cls
-  return decorator
+    def decorator(cls):
+        models[name] = cls
+        return cls
+
+    return decorator
 
 
 def make(name, **kwargs):
-  if name is None:
-    return None
-  model = models[name](**kwargs)
-  if torch.cuda.is_available():
-    model.cuda()
-  return model
+    if name is None:
+        return None
+    model = models[name](**kwargs)
+    if torch.cuda.is_available():
+        model.cuda()
+    return model
 
 
 def load(ckpt):
-  model = make(ckpt['encoder'], **ckpt['encoder_args'])
-  if model is not None:
-    model.load_state_dict(ckpt['encoder_state_dict'])
-  return model
+    model = make(ckpt["encoder"], **ckpt["encoder_args"])
+    if model is not None:
+        model.load_state_dict(ckpt["encoder_state_dict"])
+    return model
